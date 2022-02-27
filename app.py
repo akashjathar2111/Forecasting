f67a870eabcb97969ebe71a894b86190573ec199
Akash Jathar <98139627+akashjathar2111@users.noreply.github.com>
2022-02-27 22:07:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,73 +104,80 @@ else:
 
 
     elif feature == 'Quantity':
-
+        
+      
         #Total Export value of commodity per year
         Qty = data[data['Commodity']==commodity].groupby('Month')['Qty'].sum() 
         unit = data[data['Commodity']==commodity]['Unit'].unique()
-        train = Qty[:110]
-        test = Qty[110:]
+        
+        if str(unit) == ['nan']:
+            st.write("Sorry We Don't Have Data of Quantity for perticular commodity")
+        else:
+            
+        
+            train = Qty[:110]
+            test = Qty[110:]
 
-        #Arima Model
-        model = auto_arima(Qty, start_p=1, start_q=1,
-                    max_p=7, max_q=7,
-                    m=12,  ######################################           
-                    seasonal=True,   
-                    start_P=0, 
-                    D=None, 
-                    trace=True,
-                    error_action='ignore',  
-                    suppress_warnings=True, 
-                    stepwise=True)
-        pred_arima = model.predict(len(test))
-        RMSE1 = np.sqrt(mean_squared_error(test,pred_arima))            
-
-
-        #Holt Winter's Model
-        ### Holts winter exponential smoothing with additive seasonality and additive trend
-        hwe_model_add_add = ExponentialSmoothing(train,seasonal="add",trend="add",seasonal_periods=12).fit() #add the trend to the model
-        pred_hwe_add_add = hwe_model_add_add.predict(start = test.index[0],end = test.index[-1])
-        RMSE2 =np.sqrt(mean_squared_error(test,pred_hwe_add_add))
-
+            #Arima Model
+            model = auto_arima(Qty, start_p=1, start_q=1,
+                        max_p=7, max_q=7,
+                        m=12,  ######################################           
+                        seasonal=True,   
+                        start_P=0, 
+                        D=None, 
+                        trace=True,
+                        error_action='ignore',  
+                        suppress_warnings=True, 
+                        stepwise=True)
+            pred_arima = model.predict(len(test))
+            RMSE1 = np.sqrt(mean_squared_error(test,pred_arima))            
 
 
-        ### Holts winter exponential smoothing with multiplicative seasonality and additive trend
-        hwe_model_mul_add = ExponentialSmoothing(train,seasonal="mul",trend="add",seasonal_periods=12).fit() 
-        pred_hwe_mul_add = hwe_model_mul_add.predict(start = test.index[0],end = test.index[-1])
-        RMSE3 =np.sqrt(mean_squared_error(test,pred_hwe_mul_add))
-
-        st.dataframe({'Model':['ARIMA','Holt Winter with additive seasonality','Holt Winter with multiplicative seasonality'],'RMSE':[RMSE1,RMSE2,RMSE3]})
-        x = [RMSE1,RMSE2,RMSE3]
-        n = st.sidebar.slider('Forecasted',min_value=1,max_value=50)
-        best_model = pd.Series(x).min()
-        if best_model == RMSE1:
-            order = model.order
-            seasonal = model.seasonal_order
-
-            X = Qty.values
-            X = X.astype('float32')
-
-            Arima = ARIMA(X,order=order,seasonal_order=seasonal)
-            model_fit = Arima.fit()
-            forecast = model_fit.forecast(steps = n)
-            st.header(f'Quantity (in{unit})')
-            st.header("Forecasted by ARIMA Model")
-            st.line_chart(forecast)
+            #Holt Winter's Model
+            ### Holts winter exponential smoothing with additive seasonality and additive trend
+            hwe_model_add_add = ExponentialSmoothing(train,seasonal="add",trend="add",seasonal_periods=12).fit() #add the trend to the model
+            pred_hwe_add_add = hwe_model_add_add.predict(start = test.index[0],end = test.index[-1])
+            RMSE2 =np.sqrt(mean_squared_error(test,pred_hwe_add_add))
 
 
 
-        elif best_model == RMSE2:
-            forecast = hwe_model_add_add.forecast(n)
-            st.header(f'Quantity (in{unit})')
-            st.header("Forecasted by Holts winter's Additive seasonality ")
-            st.line_chart(forecast)
+            ### Holts winter exponential smoothing with multiplicative seasonality and additive trend
+            hwe_model_mul_add = ExponentialSmoothing(train,seasonal="mul",trend="add",seasonal_periods=12).fit() 
+            pred_hwe_mul_add = hwe_model_mul_add.predict(start = test.index[0],end = test.index[-1])
+            RMSE3 =np.sqrt(mean_squared_error(test,pred_hwe_mul_add))
+
+            st.dataframe({'Model':['ARIMA','Holt Winter with additive seasonality','Holt Winter with multiplicative seasonality'],'RMSE':[RMSE1,RMSE2,RMSE3]})
+            x = [RMSE1,RMSE2,RMSE3]
+            n = st.sidebar.slider('Forecasted',min_value=1,max_value=50)
+            best_model = pd.Series(x).min()
+            if best_model == RMSE1:
+                order = model.order
+                seasonal = model.seasonal_order
+
+                X = Qty.values
+                X = X.astype('float32')
+
+                Arima = ARIMA(X,order=order,seasonal_order=seasonal)
+                model_fit = Arima.fit()
+                forecast = model_fit.forecast(steps = n)
+                st.header(f'Quantity (in{unit})')
+                st.header("Forecasted by ARIMA Model")
+                st.line_chart(forecast)
 
 
-        elif best_model == RMSE3:
-            forecast = hwe_model_mul_add.forecast(n) 
-            st.header(f'Quantity (in{unit})')
-            st.header("Forecasted by Holts winter's multiplicative seasonality ")
-            st.line_chart(forecast) 
+
+            elif best_model == RMSE2:
+                forecast = hwe_model_add_add.forecast(n)
+                st.header(f'Quantity (in{unit})')
+                st.header("Forecasted by Holts winter's Additive seasonality ")
+                st.line_chart(forecast)
+
+
+            elif best_model == RMSE3:
+                forecast = hwe_model_mul_add.forecast(n) 
+                st.header(f'Quantity (in{unit})')
+                st.header("Forecasted by Holts winter's multiplicative seasonality ")
+                st.line_chart(forecast) 
 
 
 
