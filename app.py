c29d8190af6cd5653f8293d9abd18bde630c4cb1
Akash Jathar <98139627+akashjathar2111@users.noreply.github.com>
2022-02-27 23:02:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ else:
         #Total Export value of commodity per year
         Quantity = data[data['Commodity']==commodity].groupby('Month')['Qty'].sum() 
         unit = data[data['Commodity']==commodity]['Unit'].unique()
-        Qty = pd.DataFrame(round(Quantity/10000,2))
+        Qty = pd.DataFrame(round(Quantity/1000,2))
         if str(unit) == '[nan]':
             st.write("Sorry We Don't Have Data of Quantity for perticular commodity")
         else:
